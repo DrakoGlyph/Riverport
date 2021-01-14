2e7bc52240111ebf4dd8b2645e055b527187a3ff
DrakoGlyph <drakoglyph@hotmail.com>
2021-01-14 01:21:39 -0800

    Fenrir stuff

---
+++ Riverport/Fenrir/FenrirBaseCharacterCardController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Riverport.Fenrir
+{
+    class FenrirBaseCharacterCardController : HeroCharacterCardController
+    {
+        public FenrirBaseCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+        }
+
+		protected override IEnumerator RemoveCardsFromGame(IEnumerable<Card> cards)
+		{
+			if (!base.Card.IsInPlayAndHasGameText)
+			{
+				yield break;
+			}
+			IEnumerable<Card> enumerable = FindCardsWhere((Card c) => c != base.Card && c.SharedIdentifier != null && c.SharedIdentifier == base.Card.SharedIdentifier);
+			foreach (Card item in enumerable)
+			{
+				if (!item.IsIncapacitated)
+				{
+					IEnumerator coroutine = base.GameController.FlipCard(FindCardController(item), treatAsPlayed: false, treatAsPutIntoPlay: false, null, null, GetCardSource());
+					if (base.UseUnityCoroutines)
+					{
+						yield return base.GameController.StartCoroutine(coroutine);
+					}
+					else
+					{
+						base.GameController.ExhaustCoroutine(coroutine);
+					}
+				}
+			}
+			IEnumerator coroutine2 = base.RemoveCardsFromGame(cards);
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine2);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine2);
+			}
+		}
+	}
+}
---
+++ RiverportTest/ScarletXCharacterCardController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Riverport.ScarletX
+{
+    public class ScarletXCharacterCardController : HeroCharacterCardController
+    {
+        public ScarletXCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            var shoot = this.GameController.SelectAndDestroyCard(HeroTurnTakerController, new LinqCardCriteria(c => c.DoKeywordsContain("arrow")), false, null, CharacterCard, GetCardSource());
+            if(UseUnityCoroutines) { yield return this.GameController.StartCoroutine(shoot); } else { this.GameController.ExhaustCoroutine(shoot); }
+        }
+
+        public override IEnumerator UseIncapacitatedAbility(int index)
+        {
+            switch(index)
+            {
+                case 0:
+                    {
+                        var power = this.GameController.SelectHeroToUsePower(DecisionMaker, false, true, true, cardSource: GetCardSource());
+                        if(UseUnityCoroutines) { yield return this.GameController.StartCoroutine(power); } else { this.GameController.ExhaustCoroutine(power); }
+                        break;
+                    }
+                case 1:
+                    {
+                        List<DestroyCardAction> results = new List<DestroyCardAction>();
+                        var dest = this.GameController.SelectAndDestroyCard(DecisionMaker, new LinqCardCriteria(c => c.IsHero && c.IsInPlayAndHasGameText && IsEquipment(c)), true, results, CharacterCard, GetCardSource());
+                        if (UseUnityCoroutines) { yield return this.GameController.StartCoroutine(dest); } else { this.GameController.ExhaustCoroutine(dest); }
+                        if (DidDestroyCard(results))
+                        {
+                            TurnTaker tt = GetDestroyedCards(results).FirstOrDefault().Owner;
+                            if (tt is HeroTurnTaker taker)
+                            {
+                                HeroTurnTakerController httc = FindHeroTurnTakerController(taker);
+                                var damage = this.GameController.SelectTargetsAndDealDamage(httc, new DamageSource(GameController, httc.CharacterCard), 2, DamageType.Projectile, 1, false, 0, false, true, false, IsVillainTarget, cardSource: GetCardSource());
+                                if (UseUnityCoroutines) { yield return this.GameController.StartCoroutine(damage); } else { this.GameController.ExhaustCoroutine(damage); }
+                            }
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        var draw = this.GameController.SelectHeroToDrawCard(DecisionMaker, cardSource: GetCardSource());
+                        if (UseUnityCoroutines) { yield return this.GameController.StartCoroutine(draw); } else { this.GameController.ExhaustCoroutine(draw); }
+                        break;
+                    }
+            }
+        }
+    }
+}
