e6fc3a177946d9d3de970e647a3039502c4fb532
DrakoGlyph <drakoglyph@hotmail.com>
2021-01-18 18:49:37 -0800

    Strategize Rebalance

--- Riverport/Hero/Dragontamer/One-Shot/StrategizeCardController.cs
+++ Riverport/Hero/Dragontamer/One-Shot/StrategizeCardController.cs
@@ -17,8 +17,29 @@ namespace Riverport.Dragontamer
 
         public override IEnumerator Play()
         {
-            var reveal = RevealCards_MoveMatching_ReturnNonMatchingCards(TurnTakerController, TurnTaker.Deck, false, false, true, new LinqCardCriteria(c => c.DoKeywordsContain("command")), 1, null, true, false, RevealedCardDisplay.ShowMatchingCards, true);
-            if (UseUnityCoroutines) { yield return this.GameController.StartCoroutine(reveal); } else { this.GameController.ExhaustCoroutine(reveal); }
+            List<RevealCardsAction> reveal = new List<RevealCardsAction>();
+            var plan = this.GameController.RevealCards(TurnTakerController, TurnTaker.Deck, c => c.DoKeywordsContain("command"), 2, reveal, RevealedCardDisplay.ShowMatchingCards, GetCardSource());
+            if(UseUnityCoroutines) { yield return this.GameController.StartCoroutine(plan); } else { this.GameController.ExhaustCoroutine(plan); }
+            var move = BulkMoveCard(TurnTakerController, reveal.FirstOrDefault().MatchingCards, HeroTurnTaker.Hand, false, false, TurnTakerController, false);
+            if(UseUnityCoroutines) { yield return this.GameController.StartCoroutine(move); } else { this.GameController.ExhaustCoroutine(move); }
+            List<Card> observe = new List<Card>();
+            foreach(Card card in reveal.FirstOrDefault().NonMatchingCards)
+            {
+                Console.Write(card.Title + " ");
+                if(card.DoKeywordsContain("dragon"))
+                {
+                    observe.Add(card);
+                }
+            }
+            Console.WriteLine();
+            IEnumerator deploy = null;
+            if (observe.Count > 0)
+                deploy = this.GameController.SelectAndPlayCard(HeroTurnTakerController, observe, false, true, null, GetCardSource());
+            else
+                deploy = this.GameController.SendMessageAction("No Dragons came to strategize", Priority.Medium, GetCardSource(), observe);
+            if(UseUnityCoroutines) { yield return this.GameController.StartCoroutine(deploy); } else { this.GameController.ExhaustCoroutine(deploy); }
+            var clean = CleanupRevealedCards(TurnTaker.Revealed, TurnTaker.Deck, false, true, true, true);
+            if (UseUnityCoroutines) { yield return this.GameController.StartCoroutine(clean); } else { this.GameController.ExhaustCoroutine(clean); }
         }
     }
 }
