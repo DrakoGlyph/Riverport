a7fb53f62035c67e0340d7cb621e10c02647a927
DrakoGlyph <drakoglyph@hotmail.com>
2021-01-24 20:56:39 -0800

    #15 Detonation Arrow "choose for me"

--- Riverport/Hero/ScarletX/Arrows/DetonationArrowCardController.cs
+++ Riverport/Hero/ScarletX/Arrows/DetonationArrowCardController.cs
@@ -17,7 +17,7 @@ namespace Riverport.ScarletX
 
         protected override IEnumerator FireArrow(DestroyCardAction dca = null)
         {
-            var stick = this.GameController.SelectTargetsAndDealDamage(DecisionMaker, new DamageSource(GameController, CharacterCard), 2, DamageType.Projectile, 1, false, 0, false, true, false, c=>!c.IsHero, cardSource: GetCardSource());
+            var stick = this.GameController.SelectTargetsAndDealDamage(DecisionMaker, new DamageSource(GameController, CharacterCard), 2, DamageType.Projectile, 1, false, 0, false, false, false, c=>!c.IsHero, cardSource: GetCardSource());
             if (UseUnityCoroutines) { yield return this.GameController.StartCoroutine(stick); } else { this.GameController.ExhaustCoroutine(stick); }
             var detonate = this.GameController.SelectTargetsAndDealDamage(DecisionMaker, new DamageSource(GameController, CharacterCard), 1, DamageType.Fire, 3, false, 0, false, false, false, c=>!c.IsHero, cardSource: GetCardSource());
             if(UseUnityCoroutines) { yield return this.GameController.StartCoroutine(detonate); } else { this.GameController.ExhaustCoroutine(detonate); }
