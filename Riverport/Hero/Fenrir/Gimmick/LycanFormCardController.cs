ebe09c2bfba34eae4d63575f38803aa2d532a559
DrakoGlyph <drakoglyph@hotmail.com>
2021-01-29 01:10:37 -0800

    Some small balance changes to Fenrir to test. 1) Lycan Form now causes him to damage himself or self-destroy. This is to incentivise  spending time as Human Fenrir. 2) Its power is now to deal 1 target 2 Melee damage. 3) Natural Weapons is no longer a Feral card.

--- Riverport/Hero/Fenrir/Gimmick/LycanFormCardController.cs
+++ Riverport/Hero/Fenrir/Gimmick/LycanFormCardController.cs
@@ -17,7 +17,7 @@ namespace Riverport.Fenrir
         public override void AddTriggers()
         {
             AddBeforeDestroyAction(Detransform);
-
+            AddStartOfTurnTrigger(tt => tt == TurnTaker, pca => DealDamageOrDestroyThisCardResponse(pca, CharacterCard, CharacterCard, 1, DamageType.Infernal), TriggerType.DealDamage);
             AddIncreaseDamageTrigger(dda => !dda.Target.IsHero && IsFenrir(dda.DamageSource.Card), 1);
         }
 
@@ -43,7 +43,7 @@ namespace Riverport.Fenrir
 
         public override IEnumerator UsePower(int index = 0)
         {
-            var destroy = this.GameController.DestroyCard(HeroTurnTakerController, Card, cardSource: GetCardSource());
+            var destroy = this.GameController.SelectTargetsAndDealDamage(DecisionMaker, Fenrir, 2, DamageType.Melee, 1, false, 0, false, false, false, c=>!c.IsHero, cardSource:GetCardSource());
             if (UseUnityCoroutines) { yield return this.GameController.StartCoroutine(destroy); } else { this.GameController.ExhaustCoroutine(destroy); }
         }
     }
