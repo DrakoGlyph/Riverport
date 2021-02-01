eb93626db0867e843ed9b4fdbad20041dc36f231
DrakoGlyph <drakoglyph@hotmail.com>
2021-02-01 02:37:36 -0800

    Doing some cleanup on Flavor Text

--- Riverport/Hero/ArkHive/One-Shots/DeploySparesCardController.cs
+++ Riverport/Hero/ArkHive/One-Shots/DeploySparesCardController.cs
@@ -16,16 +16,13 @@ namespace Riverport.ArkHive
 
         public override IEnumerator Play()
         {
-            List<MoveCardAction> storedResults = new List<MoveCardAction>();
-            var search = this.GameController.SelectCardsFromLocationAndMoveThem(DecisionMaker, TurnTaker.Deck, 0, H - 1, new LinqCardCriteria(c => c.Identifier == "SpareNanobot"), new List<MoveCardDestination>() { new MoveCardDestination(TurnTaker.PlayArea) }, true, true, true, false, null, storedResultsMove: storedResults, selectionType: SelectionType.PutIntoPlay, cardSource: GetCardSource());
+            List<SelectCardDecision> storedResults = new List<SelectCardDecision>();
+            var search = SearchForCards(DecisionMaker, true, false, 0, H - 1, new LinqCardCriteria(c => c.Identifier == "SpareNanobot"), true, false, false, storedResults: storedResults, shuffleAfterwards: true);
             if(UseUnityCoroutines) { yield return this.GameController.StartCoroutine(search); } else { this.GameController.ExhaustCoroutine(search); }
-            int deployed = GetNumberOfCardsMoved(storedResults);
-            if (deployed > 0)
-            {
-                var pay = DealDamage(CharacterCard, CharacterCard, 2 * deployed, DamageType.Energy, cardSource: GetCardSource());
-                if(UseUnityCoroutines) { yield return this.GameController.StartCoroutine(pay); } else { this.GameController.ExhaustCoroutine(pay); }
-
-            }
+            int selected = GetNumberOfCardsSelected(storedResults);
+            Console.WriteLine("Selected = " + selected);
+            var damage = DealDamage(CharacterCard, CharacterCard, selected * 2, DamageType.Energy, cardSource: GetCardSource());
+            if(UseUnityCoroutines) { yield return this.GameController.StartCoroutine(damage); } else { this.GameController.ExhaustCoroutine(damage); }
         }
     }
 }
